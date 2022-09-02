efde7fd1db092fa28441c13ea1eac9d82f89d462
Innozenz <mfitzjean@gmail.com>
2022-09-02 17:50:06 +0200

    front only

--- backend/middleware/check-password.js
+++
@@ -1,9 +0,0 @@
-const passwordSchema = require('../passwordCheck/Models.Password');
-
-module.exports = (req, res, next) => {
-    if (!passwordSchema.validate(req.body.password)) {
-        res.status(400).json({message: "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"});
-    } else {
-        next();
-    }
-};
